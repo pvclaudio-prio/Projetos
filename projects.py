9c5acf08aa1fc04f8c58ab1a5de9651da7bb339a
pvclaudio-prio <cvieira@prio3.com.br>
2025-08-08 12:01:08 -0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -58,33 +58,6 @@ def login():
 
     return st.session_state.username, nome_usuario
 
-@st.cache_resource
-def conectar_drive():
-    cred_dict = st.secrets["credentials"]
-    credentials = OAuth2Credentials(
-        access_token=cred_dict["access_token"],
-        client_id=cred_dict["client_id"],
-        client_secret=cred_dict["client_secret"],
-        refresh_token=cred_dict["refresh_token"],
-        token_expiry=datetime.strptime(cred_dict["token_expiry"], "%Y-%m-%dT%H:%M:%SZ"),
-        token_uri=cred_dict["token_uri"],
-        user_agent="streamlit-app/1.0",
-        revoke_uri=cred_dict["revoke_uri"]
-    )
-
-    http = httplib2.Http()
-
-    try:
-        credentials.refresh(http)
-    except Exception as e:
-        st.error(f"Erro ao atualizar credenciais: {e}")
-        st.stop()
-
-    gauth = GoogleAuth()
-    gauth.credentials = credentials
-    drive = GoogleDrive(gauth)
-    return drive
-
 # 🔐 Login
 usuario_logado, nome_usuario = login()
 
